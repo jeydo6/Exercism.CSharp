1d9cfa51138d10f2df136a9a71f69de18eb5231b
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-06-16 23:36:57 +0300

    Darts

---
+++ darts/Darts.cs
@@ -0,0 +1,12 @@
+using System;
+
+internal static class Darts
+{
+    public static int Score(double x, double y) => Math.Sqrt(x * x + y * y) switch
+    {
+        <= 1 => 10,
+        <= 5 => 5,
+        <= 10 => 1,
+        _ => 0
+    };
+}
