e1a3ebb755e6e7d352929be76ebcd8994a879d27
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-06-23 10:22:56 +0300

    TracksOnTracksOnTracks

---
+++ src/tracks-on-tracks-on-tracks/TracksOnTracksOnTracks.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+internal static class Languages
+{
+    public static List<string> NewList() => new List<string>();
+
+    public static List<string> GetExistingLanguages() => new List<string> { "C#", "Clojure", "Elm" };
+
+    public static List<string> AddLanguage(List<string> languages, string language) => new List<string>(languages) { language };
+
+    public static int CountLanguages(List<string> languages) => languages.Count;
+
+    public static bool HasLanguage(List<string> languages, string language) => languages.Contains(language);
+
+    public static List<string> ReverseList(List<string> languages)
+    {
+        languages.Reverse();
+        return languages;
+    }
+
+    public static bool IsExciting(List<string> languages) =>
+        (languages.Count > 0 && languages[0] == "C#") ||
+        (languages.Count is 2 or 3 && languages[1] == "C#");
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+    {
+        languages.Remove(language);
+        return languages;
+    }
+
+    public static bool IsUnique(List<string> languages) => languages.Count == new HashSet<string>(languages).Count;
+}
