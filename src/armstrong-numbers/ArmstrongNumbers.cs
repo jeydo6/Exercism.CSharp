932929af982802e618332fc48acb2976c7793106
jeydo6 <Deryagin.Valdemar@yandex.ru>
2022-07-29 23:36:26 +0300

    ArmstrongNumbers

--- src/armstrong-numbers/ArmstrongNumbers.cs
+++ src/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,20 +1,26 @@
-using System;
-
 internal static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        var numberCopy = number;
-
-        var power = (int)Math.Ceiling(Math.Log10(numberCopy));
+        var str = number.ToString();
         
         var result = 0;
-        while (numberCopy > 0)
+        for (var i = 0; i < str.Length; i++)
         {
-            result += (int)Math.Pow(numberCopy % 10, power);
-            numberCopy /= 10;
+            result += Pow(str[i] - '0', str.Length);
         }
 
         return result == number;
+    }
+
+    private static int Pow(int number, int power)
+    {
+        var result = 1;
+        for (var i = 0; i < power; i++)
+        {
+            result *= number;
+        }
+
+        return result;
     }
 }
