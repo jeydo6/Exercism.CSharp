28c640f83808eeafd218810e27bbfb1dc235cde5
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-01-05 23:16:52 +0300

    LuciansLusciousLasagna

--- src/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ src/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,14 +1,13 @@
 internal class Lasagna
 {
-    public int ExpectedMinutesInOven() =>
-        40;
+	public int ExpectedMinutesInOven() => 40;
 
-    public int RemainingMinutesInOven(int actualMinutesInOven) =>
-        ExpectedMinutesInOven() - actualMinutesInOven;
+	public int RemainingMinutesInOven(int actualMinutesInOven) =>
+		ExpectedMinutesInOven() - actualMinutesInOven;
 
-    public int PreparationTimeInMinutes(int numberOfLayers) =>
-        numberOfLayers * 2;
+	public int PreparationTimeInMinutes(int numberOfLayers) =>
+		numberOfLayers * 2;
 
-    public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutesInOven) =>
-        PreparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
+	public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutesInOven) =>
+		PreparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
 }
