db58b3a7c57672e4d548a2b04bd55d5cc51516bc
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-01-05 23:19:59 +0300

    SqueakyClean

--- src/squeaky-clean/SqueakyClean.cs
+++ src/squeaky-clean/SqueakyClean.cs
@@ -2,30 +2,30 @@ using System.Text;
 
 internal static class Identifier
 {
-    public static string Clean(string identifier)
-    {
-        var sb = new StringBuilder();
-        for (var i = 0; i < identifier.Length; i++)
-        {
-            var ch = identifier[i];
-            if (ch == ' ')
-            {
-                sb.Append('_');
-            }
-            else if (char.IsControl(ch))
-            {
-                sb.Append("CTRL");
-            }
-            else if (ch == '-')
-            {
-                sb.Append(char.ToUpper(identifier[++i]));
-            }
-            else if (char.IsLetter(ch) && ch is not (>= 'α' and <= 'ω'))
-            {
-                sb.Append(ch);
-            }
-        }
+	public static string Clean(string identifier)
+	{
+		var sb = new StringBuilder();
+		for (var i = 0; i < identifier.Length; i++)
+		{
+			var ch = identifier[i];
+			if (ch == ' ')
+			{
+				sb.Append('_');
+			}
+			else if (char.IsControl(ch))
+			{
+				sb.Append("CTRL");
+			}
+			else if (ch == '-')
+			{
+				sb.Append(char.ToUpper(identifier[++i]));
+			}
+			else if (char.IsLetter(ch) && ch is not (>= 'α' and <= 'ω'))
+			{
+				sb.Append(ch);
+			}
+		}
 
-        return sb.ToString();
-    }
+		return sb.ToString();
+	}
 }
