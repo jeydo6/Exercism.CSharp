9d3eee0dedf9b457915e05c253203e84e2d47ee2
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-01-05 23:17:39 +0300

    AnnalynsInfiltration

--- src/annalyns-infiltration/AnnalynsInfiltration.cs
+++ src/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -1,15 +1,15 @@
 internal static class QuestLogic
 {
-    public static bool CanFastAttack(bool knightIsAwake) =>
-        !knightIsAwake;
+	public static bool CanFastAttack(bool knightIsAwake) =>
+		!knightIsAwake;
 
-    public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) =>
-        knightIsAwake || archerIsAwake || prisonerIsAwake;
+	public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake) =>
+		knightIsAwake || archerIsAwake || prisonerIsAwake;
 
-    public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) =>
-        !archerIsAwake && prisonerIsAwake;
+	public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake) =>
+		!archerIsAwake && prisonerIsAwake;
 
-    public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) =>
-        (petDogIsPresent && !archerIsAwake) ||
-        (!petDogIsPresent && prisonerIsAwake && !archerIsAwake && !knightIsAwake);
+	public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent) =>
+		(petDogIsPresent && !archerIsAwake) ||
+		(!petDogIsPresent && prisonerIsAwake && !archerIsAwake && !knightIsAwake);
 }
