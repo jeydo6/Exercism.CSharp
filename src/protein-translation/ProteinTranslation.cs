cbe5a29f6c9056d040914552732a5fe65e5ba43a
jeydo6 <Deryagin.Valdemar@yandex.ru>
2023-02-23 00:00:59 +0300

    ProteinTranslation

--- src/protein-translation/ProteinTranslation.cs
+++ src/protein-translation/ProteinTranslation.cs
@@ -1,40 +1,24 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 internal static class ProteinTranslation
 {
-    private static readonly IDictionary<string, string> _translations = new Dictionary<string, string>
-    {
-        ["AUG"] = "Methionine",
-        ["UUU"] = "Phenylalanine",
-        ["UUC"] = "Phenylalanine",
-        ["UUA"] = "Leucine",
-        ["UUG"] = "Leucine",
-        ["UCU"] = "Serine",
-        ["UCC"] = "Serine",
-        ["UCA"] = "Serine",
-        ["UCG"] = "Serine",
-        ["UAU"] = "Tyrosine",
-        ["UAC"] = "Tyrosine",
-        ["UGU"] = "Cysteine",
-        ["UGC"] = "Cysteine",
-        ["UGG"] = "Tryptophan",
-        ["UAA"] = "STOP",
-        ["UAG"] = "STOP",
-        ["UGA"] = "STOP"
-    };
-    
     public static string[] Proteins(string strand)
     {
-        var proteins = new List<string>();
-        
-        var i = 0;
-        while (i < strand.Length && _translations[strand.Substring(i, 3)] != "STOP")
-        {
-            var codon = strand.Substring(i, 3);
-            proteins.Add(_translations[codon]);
-            i += codon.Length;
-        }
-
-        return proteins.ToArray();
+        var terminatingCodons = new HashSet<string> { "UAA", "UAG", "UGA" };
+        return strand.Chunk(3).Select(x => new string(x))
+            .TakeWhile(i => !terminatingCodons.Contains(i))
+            .Select(i => i switch
+            {
+                "AUG" => "Methionine",
+                "UUU" or "UUC" => "Phenylalanine",
+                "UUA" or "UUG" => "Leucine",
+                "UCU" or "UCC" or "UCA" or "UCG" => "Serine",
+                "UAU" or "UAC" => "Tyrosine",
+                "UGU" or "UGC" => "Cysteine",
+                "UGG" => "Tryptophan",
+                _ => throw new ArgumentOutOfRangeException()
+            }).ToArray();
     }
 }
